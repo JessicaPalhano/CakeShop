c4d6ba4c505b9b43a95e16630aff796146280ddf
Jessica <58232904+JessicaPalhano@users.noreply.github.com>
2022-09-05 20:10:25 -0300

    Responsividade para web

--- main.js
+++ main.js
@@ -5,6 +5,39 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+
+  //Verificar se chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  //Verificar se a base está abaixo da linha alvo
+  //Onde a seção termina
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  //O final da seção passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  //Limites da seção
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  //Primeiro remove, depois adiciona
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
@@ -37,9 +70,9 @@ ScrollReveal({
   origin: 'bottom',
   distance: '30px',
   duration: 700
-}).reveal(`#home, 
-#home img, 
-#home .stats, 
+}).reveal(`#home,
+#home img,
+#home .stats,
 #services,
 #services header,
 #services .card,
